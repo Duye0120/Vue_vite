59b75578ca3ad09f4da28bf188b47889926b1627
Duye <1939034592@qq.com>
2022-03-29 22:42:03 +0800

    feature(learn):
    - slot学习
    - router-view学习
    - Arco添加引用

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ```cmd
 pnpm i @types/node -D
 ```
+
 - 修改vite.config.js
 
 ```js
@@ -27,6 +28,7 @@ export default defineConfig({
 })
 
 ```
+
 - 修改tsconfig.json
 
 ```json
@@ -41,3 +43,37 @@ export default defineConfig({
     }
 }
 ```
+
+#Day 2
+
+## 第一个问题,如果通过Slots来控制content内容
+
+---
+
+知识点
+
+- 使用setup语法以后,可以不用导出component 可以直接使用
+- `<router-view/>`被用在layout中
+- v-slot可以用来自定义插槽内容
+
+![](image/README/1648560768964.png)
+
+### `<Slot>插槽学习`
+
+```javascript
+<FrancyButton>
+  // 插槽内容
+  Clik me!
+</FrancyButton>
+
+
+// 这个组件的模板是
+
+<button>
+  <slot></slot>
+</button>
+```
+
+slot是插口,表示父元素的插槽内容将在哪里被渲染.
+
+插槽中可以放默认值,意味着,当插槽中没有值的时候,可以有默认值,类似于,emmm,`<Empty/>`?
--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,16 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
+<template>
+  <Layouts />
+</template>
+<script lang="ts" setup>
+import Layouts from '@/layouts/index.vue';
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <router-link to="/">home</router-link>|
-  <router-link to="/about">about</router-link>
-  <a href="/about">a tag about</a>
-  <router-view></router-view>
-</template>
-
+  <!-- <router-view></router-view> -->
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
+  width: 100%;
 }
 </style>
