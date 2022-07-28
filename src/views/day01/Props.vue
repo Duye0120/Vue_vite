1bf0fc886f80c9148afd56f6fac6f12c4f622edf
Duye <1939034592@qq.com>
2022-07-28 12:06:49 +0800

    feat: a

--- src/views/day01/Props.vue
+++ src/views/day01/Props.vue
@@ -5,14 +5,15 @@
 </template>
 
 <script lang="ts" setup>
-import Student from '@/components/Student.vue';
-import { watch } from 'fs';
-import { ref } from 'vue';
-const name = '杜也';
-const age = 18;
-const sex = '男';
+  import { watch } from 'fs';
+  import { ref } from 'vue';
+  import Student from '@/components/Student.vue';
+  const name = '杜也';
+  const age = 18;
+  const sex = '男';
+  const address = '南京';
 </script>
 
-<style lang='less' scoped>
-//@import url(); 引入公共css类
+<style lang="less" scoped>
+  //@import url(); 引入公共css类
 </style>
